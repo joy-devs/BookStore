344db021e3adef1c86b860b5c584ae83b1ffbee0
daniel-kav <dankavatha01@gmail.com>
2024-06-26 15:17:30 +0300

    added a book relation

--- src/Reducers/Bookreducer.tsx
+++ src/Reducers/Bookreducer.tsx
@@ -1,25 +1,25 @@
-import { Book } from "../components/BookRepository";
+// import { Book } from "../components/BookRepository";
 
 
-export type Action =
-  | { type: 'SET_INITIAL_STATE'; payload: Book[] }
-  | { type: 'ADD_BOOK'; payload: Book }
-  | { type: 'UPDATE_BOOK'; payload: Book }
-  | { type: 'DELETE_BOOK'; payload: string };
+// export type Action =
+//   | { type: 'SET_INITIAL_STATE'; payload: Book[] }
+//   | { type: 'ADD_BOOK'; payload: Book }
+//   | { type: 'UPDATE_BOOK'; payload: Book }
+//   | { type: 'DELETE_BOOK'; payload: string };
 
-const bookReducer = (state: Book[], action: Action): Book[] => {
-  switch (action.type) {
-    case 'SET_INITIAL_STATE':
-      return action.payload;
-    case 'ADD_BOOK':
-      return [...state, action.payload];
-    case 'UPDATE_BOOK':
-      return state.map(book => (book.id === action.payload.id ? action.payload : book));
-    case 'DELETE_BOOK':
-      return state.filter(book => book.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const bookReducer = (state: Book[], action: Action): Book[] => {
+//   switch (action.type) {
+//     case 'SET_INITIAL_STATE':
+//       return action.payload;
+//     case 'ADD_BOOK':
+//       return [...state, action.payload];
+//     case 'UPDATE_BOOK':
+//       return state.map(book => (book.id === action.payload.id ? action.payload : book));
+//     case 'DELETE_BOOK':
+//       return state.filter(book => book.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
 
-export default bookReducer;
+// export default bookReducer;
